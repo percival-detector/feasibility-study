bcb131284b993ffd9994aac1516ac3e5658fbc01
Alan Greer <Alan.Greer@diamond.ac.uk>
2013-09-02 16:43:21 +0000

    Replace a single test application with the generator and Percival test camera applications.  Update library dependencies as required by the use of the areaDetector module.

--- etc/builder.py
+++ etc/builder.py
@@ -3,6 +3,8 @@ from iocbuilder import Device, AutoSubstitution, Architecture, SetSimulation
 from iocbuilder.arginfo import *
 from iocbuilder.modules.asyn import Asyn, AsynPort, AsynIP
 
+from iocbuilder.modules.areaDetector import AreaDetector, _NDPluginBase, _ADBase, _ADBaseTemplate, simDetector
+
 class _PercivalConfigurator(AutoSubstitution):
     TemplateFile="PercivalConfigurator.template"
 
@@ -27,7 +29,7 @@ class PercivalConfigurator(AsynPort):
         
     # __init__ arguments
     ArgInfo = _BaseTemplate.ArgInfo
-    LibFileList = ['percival']
+    LibFileList = ['percGen']
     DbdFileList = ['PercivalConfigurator']
     SysLibFileList = []
     MakefileStringList = []
@@ -62,7 +64,7 @@ class PercivalGenerator(AsynPort):
         
     # __init__ arguments
     ArgInfo = _BaseTemplate.ArgInfo
-    LibFileList = ['percival']
+    LibFileList = ['percGen']
     DbdFileList = ['PercivalGenerator']
     SysLibFileList = []
     MakefileStringList = []
@@ -71,5 +73,34 @@ class PercivalGenerator(AsynPort):
     def Initialise(self):
         print '# GeneratorInit( portName )'
         print 'GeneratorInit( %(PORT)s )' % self.__dict__
+
+
+class _ADPercivalDriver(AutoSubstitution):
+    TemplateFile="ADPercivalDriver.template"
+    SubstitutionOverwrites = [_ADBaseTemplate]
+
+class ADPercivalDriver(_ADBase):
+    """Create a Percival camera detector"""
+    _SpecificTemplate = _ADPercivalDriver
+    AutoInstantiate = True
+    def __init__(self, BUFFERS=50, MEMORY=-1, **args):
+        self.__super.__init__(**args)
+        self.__dict__.update(locals())
+        
+
+    # __init__ arguments
+    ArgInfo = _ADBase.ArgInfo + _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
+        BUFFERS = Simple('Maximum number of NDArray buffers to be created for '
+            'plugin callbacks', int),
+        MEMORY  = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
+            'for driver and all attached plugins', int))
+    LibFileList = ['percival']
+    DbdFileList = ['ADPercivalDriver']
+    SysLibFileList = []
+    MakefileStringList = []
+
+    def Initialise(self):
+        print '# PercivalInit( portName, maxBuffers, maxMemory )'
+        print '  PercivalInit( %(PORT)10s, %(BUFFERS)10d, %(MEMORY)9d )' % self.__dict__
 
 
